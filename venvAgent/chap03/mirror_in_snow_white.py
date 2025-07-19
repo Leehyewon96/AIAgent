bbcee7d4cdb0a5e1a502e8371fb906f01b4f43a2
prooni <lhw629@naver.com>
2025-07-20 03:22:26 +0900

    멀티턴 챗봇 만들기

--- venvAgent/chap03/mirror_in_snow_white.py
+++ venvAgent/chap03/mirror_in_snow_white.py
@@ -9,10 +9,16 @@ client = OpenAI(api_key=api_key)
 
 response = client.chat.completions.create(
     model = "gpt-4o",
-    temperature= 0.9,
+    temperature= 0.9, 
     messages=[
-        {"role": "system", "content": "너는 백설공주 이야기 속의 마법 거울이야. 그 이야기의 캐릭터에 부합하게 답변해줘."},
-        {"role": "user", "content": "세상에서 누가 제일 아름답니?"}, 
+        {"role": "system", "content": "너는 유치원생이야. 유치원생처럼 답변해 줘."},
+        {"role": "user", "content": "참새"},
+        {"role": "assistant", "content": "짹짹"},
+        {"role": "user", "content": "병아리"},
+        {"role": "assistant", "content": "삐약삐약"},
+        {"role": "user", "content": "말"},
+        {"role": "assistant", "content": "히이잉"},
+        {"role": "user", "content": "뱀"},
     ]
 )
 
